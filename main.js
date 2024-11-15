5fe6df145d42f4bb790be3298e46c25eb464d415
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-15 11:42:41 +0100

    feat: 🎸  implemented encode function and tested it

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 import getLatinCharacterList from "./javascript/functions/getLatinCharacterList.js";
 import translateLatinCharacter from "./javascript/functions/translateLatinCharacter.js";
+import encode from "./javascript/functions/encode.js";
 
 console.log(getLatinCharacterList("Hello, world"));
 console.log(translateLatinCharacter("A"));
+console.log(encode("Hello, world"));
