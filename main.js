5f92fad0403fe5716ac1fb7ae3ca382bbc288ab2
Ange230700 <kouakouangeericstephane@gmail.com>
2024-11-15 10:52:38 +0100

    feat: 🎸  implemented translateLatinCharacter function

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // main.js
 
 import getLatinCharacterList from "./javascript/functions/getLatinCharacterList.js";
+import translateLatinCharacter from "./javascript/functions/translateLatinCharacter.js";
 
 console.log(getLatinCharacterList("Hello, world"));
+console.log(translateLatinCharacter("A"));
